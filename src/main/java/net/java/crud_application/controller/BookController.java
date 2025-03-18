4f1a664669ad1a9f1cc42a6d37ad5a886b98648f
dheerajoy <dheerajsuthar46@gmail.com>
2025-03-18 23:42:09 +0530

    added custom-exception and separated service and controller code

--- src/main/java/net/java/crud_application/controller/BookController.java
+++ src/main/java/net/java/crud_application/controller/BookController.java
@@ -4,14 +4,12 @@ import net.java.crud_application.model.Book;
 import net.java.crud_application.repo.BookRepo;
 import net.java.crud_application.service.BookService;
 import net.java.crud_application.utils.ResponseUtil;
+import net.java.crud_application.utils.ValidationUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 @RestController
 public class BookController {
@@ -25,67 +23,54 @@ public class BookController {
     @Autowired
     private ResponseUtil responseUtil;
 
+    @Autowired
+    private ValidationUtil validationUtil;
+
     @GetMapping("/getAllBooks")
     public ResponseEntity<Map<String, Object>> getAllBooks(){
         try {
-            List<Book> books = bookService.getAllBooks();
-
-            if(books.isEmpty()){
-                return responseUtil.customFailureResponse("No data available");
-            }
-
-            return responseUtil.customSuccessResponse(books);
-        } catch(Exception ex){
+            return responseUtil.customSuccessResponse(bookService.getAllBooks());
+        } catch (Exception ex) {
             return responseUtil.customFailureResponse(ex.getMessage());
         }
     }
 
     @GetMapping("/getBookById/{id}")
-    public ResponseEntity<Book> getBookById(@PathVariable Long id){
-        Optional<Book> bookData = bookRepo.findById(id);
-
-        if(bookData.isPresent()){
-        return new ResponseEntity<>(bookData.get(),HttpStatus.OK);
+    public ResponseEntity<Map<String, Object>> getBookById(@PathVariable Long id){
+        try{
+            return responseUtil.customSuccessResponse(bookService.getBookById(id));
+        } catch (Exception e) {
+            return responseUtil.customFailureResponse(e.getMessage());
         }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PostMapping("/addBook")
-    public ResponseEntity<Book> addBook(@RequestBody Book book){
+    public ResponseEntity<Map<String, Object>> addBook(@RequestBody Book book){
         try{
-
-          Book bookObj = bookRepo.save(book);
-          return new ResponseEntity<>(bookObj,HttpStatus.OK);
-
+            validationUtil.validateBookRequest(book);
+            return responseUtil.customSuccessResponse(bookService.addBook(book));
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            return responseUtil.customFailureResponse(e.getMessage());
         }
     }
 
     @PostMapping("/updateBookById/{id}")
-    public ResponseEntity<Book> updateBookById(@PathVariable long id, @RequestBody Book newBookData){
-        Optional<Book> oldBookData = bookRepo.findById(id);
-        if (oldBookData.isPresent()){
-            Book updatedBookData = oldBookData.get();
-            updatedBookData.setAuthor(newBookData.getAuthor());
-            updatedBookData.setTitle(newBookData.getTitle());
-
-            Book bookObj =  bookRepo.save(updatedBookData);
-            return new ResponseEntity<>(bookObj, HttpStatus.OK);
+    public ResponseEntity<Map<String, Object>> updateBookById(@PathVariable long id, @RequestBody Book newBookData){
+        try{
+            validationUtil.validateBookRequest(newBookData);
+            return responseUtil.customSuccessResponse(bookService.updateBookById(id, newBookData));
+        } catch (Exception e) {
+            return responseUtil.customFailureResponse(e.getMessage());
         }
-
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping("/deleteBookById/{id}")
-    public ResponseEntity<HttpStatus> deleteBookById(@PathVariable long id){
-        Optional<Book> oldBookData = bookRepo.findById(id);
-
-        if(oldBookData.isPresent()){
-            bookRepo.deleteById(id);
-            return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<Map<String,Object>> deleteBookById(@PathVariable long id){
+        try{
+            bookService.deleteBookById(id);
+            return responseUtil.customSuccessResponse("Deleted Book Successfully");
+        } catch (Exception e) {
+            return responseUtil.customFailureResponse(e.getMessage());
         }
-
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/net/java/crud_application/service/BookService.java
+++ src/main/java/net/java/crud_application/service/BookService.java
@@ -2,10 +2,12 @@ package net.java.crud_application.service;
 
 import net.java.crud_application.model.Book;
 import net.java.crud_application.repo.BookRepo;
+import net.java.crud_application.utils.CustomException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookService {
@@ -15,6 +17,46 @@ public class BookService {
 
     // Fetch all books
     public List<Book> getAllBooks() {
-        return bookRepo.findAll();
+        List<Book> books = bookRepo.findAll();
+        if (books.isEmpty()) {
+            throw new CustomException("No books found");
+        }
+        return books;
+    }
+
+    // Get a Book by Id
+    public Book getBookById(Long id){
+        Optional<Book> book = bookRepo.findById(id);
+        if (!book.isPresent()) {
+            throw new CustomException("Book not found");
+        }
+        return book.get();
+    }
+
+    // Add book
+    public Book addBook(Book book){
+        return bookRepo.save(book);
+    }
+
+    // Update Book
+    public Book updateBookById(Long id, Book newBookData){
+        Optional<Book> oldBookData = bookRepo.findById(id);
+        if(oldBookData.isPresent()){
+            Book updateBookData = oldBookData.get();
+            updateBookData.setAuthor(newBookData.getAuthor());
+            updateBookData.setTitle(newBookData.getTitle());
+            return bookRepo.save(updateBookData);
+        }else{
+            throw new CustomException("Book cannot be updated");
+        }
+    }
+
+    public void deleteBookById(Long id){
+        Optional<Book> book = bookRepo.findById(id);
+        if (book.isPresent()){
+            bookRepo.deleteById(id);
+        }else{
+            throw new CustomException("Cannot Delete the book");
+        }
     }
 }
